eec6b211b5e3f2734f86eee7c1057bc906c2c4fe
Skip Hansen <skip@gfrn.org>
2024-09-07 10:43:30 -0700

    Added support for FW version v0011, fixed bug patching full files which clobbered the first 4 bytes of the image.

--- Chroma_Tag_FW/patch_sn/patch_sn.py
+++ Chroma_Tag_FW/patch_sn/patch_sn.py
@@ -16,7 +16,7 @@ def patch_sn(file_path, sn):
 
     try:
         if file_path.lower().endswith('.hex'):
-            print("Patching full hex file")
+            file_type = "full hex"
             hex_file = True
             ih = IntelHex(file_path)
             bin = bytearray(ih.tobinarray())
@@ -28,7 +28,7 @@ def patch_sn(file_path, sn):
             with open(file_path, "rb") as file:
                 bin = bytearray(file.read())
                 if file_size == 32768:
-                    print("Patching full binary file")
+                    file_type = "full binary"
                     binary_file = True
                     pass
                 else:
@@ -36,7 +36,7 @@ def patch_sn(file_path, sn):
                     if tag_type == 'chroma':
                         # ota file
                         if bin[8] == 1:
-                            print("Patching ota file")
+                            file_type = "OTA"
                             ota_offset = 25
                             ota_file = True
                         else:
@@ -46,19 +46,29 @@ def patch_sn(file_path, sn):
         SnOffset = (bin[32767 + ota_offset] << 8) + bin[32766 + ota_offset]
         SnOffset += ota_offset
         MagicOffset = SnOffset - 6
-        NvMagic = bytearray([0x56, 0x12, 0x09, 0x85])
-        if bin[MagicOffset:MagicOffset+4] != NvMagic:
+        NvMagic = bytearray([0x56, 0x12, 0x09, 0x85, 0x01, 0x08])
+        SnOffset = bin.find(NvMagic)
+
+        if SnOffset < 0:
             print(f'NVRAM magic not found')
             binary_file = False
             ota_file = False
             raise SnException()
 
+        SnOffset += 6
         tag_type = bin[SnOffset:SnOffset+2].decode("utf-8")
+
+        if sn is None:
+            print(f'SN is {tag_type}{bin[SnOffset+2:SnOffset+6].hex()}')
+            return
+
+        sn = sn.upper()
         if sn[0:2] != tag_type:
             print(f'Invalid SN, first two letters much be "{tag_type}" for this tag type')
             raise SnException()
+
         new_sn = bytes.fromhex(sn[2:10])
-        print(f'SN changed from {tag_type}{bin[SnOffset+2:SnOffset+6].hex()} to {sn}')
+        print(f'SN in {file_type} file changed from {tag_type}{bin[SnOffset+2:SnOffset+6].hex()} to {sn}')
         bin[SnOffset+2:SnOffset+6] = new_sn
         if hex_file:
             ih = IntelHex()
@@ -68,9 +78,10 @@ def patch_sn(file_path, sn):
             with open(file_path, "r+b") as file:
                 file.seek(SnOffset+2)
                 file.write(new_sn)
-                crc = zlib.crc32(bin[ota_offset:])
-                file.seek(0)
-                file.write(crc.to_bytes(4,'little'))
+                if ota_file:
+                    crc = zlib.crc32(bin[ota_offset:])
+                    file.seek(0)
+                    file.write(crc.to_bytes(4,'little'))
                 file.close()
 
     except FileNotFoundError:
@@ -85,15 +96,16 @@ def sn_help():
     print('The SN must be 2 letters, 8 digits and one letter. For example "JM10339094B".')
 
 def validate_arguments(args):
-    result = False
-    SN = args.SN.upper()
-    sn_len = len(SN)
-    if sn_len != 10 and sn_len != 11:
-        pass
-    elif not SN[0:2].isalpha() or not SN[2:10].isdigit():
+    result = True
+    if args.SN is None:
         pass
     else:
-        result = True
+        SN = args.SN.upper()
+        sn_len = len(SN)
+        if sn_len != 10 and sn_len != 11:
+            result = False
+        elif not SN[0:2].isalpha() or not SN[2:10].isdigit():
+            result = False
 
     if not result:
         sn_help()
@@ -102,16 +114,15 @@ def validate_arguments(args):
 
 def main():
     parser = argparse.ArgumentParser(description="Patch serial number in a binary firmware file for a Chroma tag")
-    parser.add_argument("SN", help="New Serial Number")
     parser.add_argument("filename", help="Binary file to patch (.bin or .hex)")
+    parser.add_argument("SN", nargs='?', help="New Serial Number")
 
     args = parser.parse_args()
-
 
     if not validate_arguments(args):
         return
     
-    patch_sn(args.filename, args.SN.upper())
+    patch_sn(args.filename, args.SN)
 
 if __name__ == "__main__":
     main()
